595e6dd3d7184b1473ddb6ffe13a06db1593f29e
bschmeling <benjamin.schmeling@gmail.com>
2020-05-29 14:42:14 +0200

    Implemented remote hotrod access, values are plain strings

--- components/kafka2datagrid-service/src/main/java/com/redhat/bobbycar/routes/KafkaToDatagridRoute.java
+++ components/kafka2datagrid-service/src/main/java/com/redhat/bobbycar/routes/KafkaToDatagridRoute.java
@@ -1,33 +1,64 @@
 package com.redhat.bobbycar.routes;
-import java.util.Properties;
+import java.nio.charset.Charset;
 
+import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.infinispan.InfinispanConstants;
 import org.apache.camel.component.infinispan.InfinispanOperation;
+import org.infinispan.client.hotrod.configuration.ClientIntelligence;
+import org.infinispan.client.hotrod.configuration.Configuration;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.configuration.SaslQop;
+import org.infinispan.commons.marshall.StringMarshaller;
 
 public class KafkaToDatagridRoute extends RouteBuilder {
 	
+	@PropertyInject("com.redhat.bobbycar.camelk.dg.host")
+    private String datagridHost;
+	@PropertyInject(value = "com.redhat.bobbycar.camelk.dg.user", defaultValue = "developer")
+    private String datagridUsername;
+	@PropertyInject("com.redhat.bobbycar.camelk.dg.password")
+    private String datagridPassword;
+	
 	@Override
 	public void configure() throws Exception {
-		Properties properties = new Properties();
-		properties.put("infinispan.client.hotrod.auth_username", propertyInject("{{com.redhat.bobbycar.camelk.dg.user}}", String.class));
-		properties.put("infinispan.client.hotrod.auth_password", propertyInject("{{com.redhat.bobbycar.camelk.dg.password}}", String.class));
-		ConfigurationBuilder hotRodBuilder = new ConfigurationBuilder();
-		hotRodBuilder.addServer()
-	        .host(propertyInject("{{com.redhat.bobbycar.camelk.dg.host}}", String.class))
-	        .port(9999)
-	        .withProperties(properties)
-	        .version(org.infinispan.client.hotrod.ProtocolVersion.PROTOCOL_VERSION_30)
-	    .build();
+		bindToRegistry("cacheContainerConfiguration", getCacheConfig());
 		
-		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic}}?clientId=kafkaSseCamelClient&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}:9092")
+		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic}}?clientId=kafkaToDatagridCamelClient&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}:9092")
 			.setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.PUT)
-		    .setHeader(InfinispanConstants.KEY).expression(simple("${headers[kafka.KEY]}"))
+			.setHeader(InfinispanConstants.KEY).expression(jsonpath("$.carid"))
+			/*.process((ex) -> {
+				System.out.println(ex.getIn().getBody().getClass());
+				ex.getIn().setBody(ex.getIn().getBody().toString());
+				ex.getIn().setBody("{\"_type\":\"Car\"," + ex.getIn().getBody().toString().substring(1));
+			})*/
 		    .setHeader(InfinispanConstants.VALUE).expression(simple("${body}"))
-		    .log("Saving data to cache with key: ${headers[kafka.KEY]} and value: ${body}")
-			.to("infinispan://{{com.redhat.bobbycar.camelk.dg.cacheName}}");
+		    
+		    .log("Saving data to cache with key: ${headers[CamelInfinispanKey]} and value: ${body} of type  ${body.class}")
+			.to("infinispan://{{com.redhat.bobbycar.camelk.dg.cacheName}}?cacheContainerConfiguration=#cacheContainerConfiguration");
 	}
 	
+	private Configuration getCacheConfig() throws Exception {
+		ConfigurationBuilder hotRodBuilder = new ConfigurationBuilder();
+
+		return hotRodBuilder.addServer()
+	        .host(datagridHost).port(11222)
+	        	.marshaller(new StringMarshaller(Charset.defaultCharset()))
+	        	//.marshaller(new ProtoStreamMarshaller())
+	        .clientIntelligence(ClientIntelligence.BASIC)
+	        	.security()
+	        		.authentication().enable()
+	        		.username(datagridUsername)
+	        		.password(datagridPassword)
+	        		.realm("default")
+	        		.serverName("infinispan")
+	        		.saslQop(SaslQop.AUTH)
+	        		.saslMechanism("DIGEST-MD5")
+	        		
+			.ssl()
+	        	.sniHostName(datagridHost)
+	        	.trustStorePath("/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt")
+        .build();
+	}
 	
 }
