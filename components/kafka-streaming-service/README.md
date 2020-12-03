5c179e931363ddbdebc720ddafc15c6f2a13cd8d
Ortwin Schneider <ortwin.schneider@googlemail.com>
2020-12-03 14:25:51 +0100

    add /carmetrics Websocket endpoint

--- components/kafka-streaming-service/README.md
+++ components/kafka-streaming-service/README.md
@@ -2,4 +2,4 @@
 
 ## Dev
 
-`kamel run src/main/java/com/redhat/bobbycar/routes/KafkaSseRoute.java --dev --property com.redhat.bobbycar.camelk.kafka.topic=bobbycar-gps -d camel-undertow --profile=openshift`
+`kamel run src/main/java/com/redhat/bobbycar/routes/KafkaSseRoute.java --dev --name dashboardStreamingDev --secret=kafka-streaming-secret --profile=openshift`
--- components/kafka-streaming-service/src/main/java/com/redhat/bobbycar/routes/KafkaSseRoute.java
+++ components/kafka-streaming-service/src/main/java/com/redhat/bobbycar/routes/KafkaSseRoute.java
@@ -5,11 +5,21 @@ public class KafkaSseRoute extends RouteBuilder {
 	
 	@Override
 	public void configure() throws Exception {
-		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic}}?clientId=kafkaSseCamelClient&brokers=bobbycar-cluster-kafka-brokers:9092")
-			.log("Message received from Kafka : ${body}")
+
+		// expose Bobbycar GPS positions from Kafka as Websocket
+		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic.gps}}?clientId=kafkaSseCamelClient&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}")
+			.log("GPS position received from Kafka : ${body}")
 			.to("undertow:ws://0.0.0.0:8080/api/carevents?sendToAll=true");
 		from("undertow:ws://0.0.0.0:8080/api/carevents?sendToAll=true")
 			.log("Message received from Websocket : ${body}");
+
+		// expose Bobbycar engine metrics from Kafka as Websocket
+		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic.metrics}}?clientId=kafkaSseMetricsClient&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}")
+			.log("Metric received from Kafka : ${body}")
+			.to("undertow:ws://0.0.0.0:8080/api/carmetrics?sendToAll=true");
+		from("undertow:ws://0.0.0.0:8080/api/carmetrics?sendToAll=true")
+			.log("Message received from Websocket : ${body}");
+		
 	}
 	
 	
