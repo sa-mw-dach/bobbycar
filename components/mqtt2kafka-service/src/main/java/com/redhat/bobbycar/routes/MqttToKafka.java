724627562c90d77e4b243fea11df67115874246b
Ortwin Schneider <ortwin.schneider@googlemail.com>
2021-06-28 17:01:07 +0200

    set Kafka message key, add hdpp endpoint

--- components/mqtt2kafka-service/src/main/java/com/redhat/bobbycar/routes/MqttToKafka.java
+++ components/mqtt2kafka-service/src/main/java/com/redhat/bobbycar/routes/MqttToKafka.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
 
 public class MqttToKafka extends RouteBuilder {
 	
@@ -13,6 +14,8 @@ public class MqttToKafka extends RouteBuilder {
 
 		from("paho:{{com.redhat.bobbycar.camelk.mqtt.topic}}?brokerUrl={{com.redhat.bobbycar.camelk.mqtt.brokerUrl}}")
 			.log("Publishing engine metric ${body} to Kafka")
+			.setHeader(KafkaConstants.KEY).expression(jsonpath("$.driverId"))
+			//.log("Publishing engine metric ${headers} to Kafka")
 		.to("kafka:{{com.redhat.bobbycar.camelk.kafka.topic}}?clientId=mqtt2kafkaClientEM&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}");
 
 		from("paho:{{com.redhat.bobbycar.camelk.mqtt.topicZoneChange}}?brokerUrl={{com.redhat.bobbycar.camelk.mqtt.brokerUrl}}")
