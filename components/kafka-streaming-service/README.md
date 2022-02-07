b18a973ade1459969e4be03761908f53792f8d43
Ortwin Schneider <ortwin.schneider@googlemail.com>
2022-02-07 12:17:16 +0100

    Replaced undertow with vertx-websocket. Bumped version.

--- components/kafka-streaming-service/README.md
+++ components/kafka-streaming-service/README.md
@@ -1,5 +1,7 @@
 # kafka-streaming-service
 
-## Dev
+## Dev mode
 
-`kamel run src/main/java/com/redhat/bobbycar/routes/KafkaSseRoute.java --dev --name dashboardStreamingDev --secret=dashboard-streaming-service-secret --profile=openshift`
+````shell
+kamel run src/main/java/com/redhat/bobbycar/routes/KafkaSseRoute.java --dev --name kafkaStreamingDev --secret=kafka-streaming --profile=openshift --trait service.enabled=false
+````
--- components/kafka-streaming-service/src/main/java/com/redhat/bobbycar/routes/KafkaSseRoute.java
+++ components/kafka-streaming-service/src/main/java/com/redhat/bobbycar/routes/KafkaSseRoute.java
@@ -1,5 +1,6 @@
 package com.redhat.bobbycar.routes;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.vertx.websocket.VertxWebsocketConstants;
 
 public class KafkaSseRoute extends RouteBuilder {
 	
@@ -7,25 +8,29 @@ public class KafkaSseRoute extends RouteBuilder {
 	public void configure() throws Exception {
 
 		// expose Bobbycar GPS positions from Kafka as Websocket
-		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic.gps}}?clientId=kafkaSseCamelClient&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}")
-			.log("GPS position received from Kafka : ${body}")
-			.to("undertow:ws://0.0.0.0:8080/api/carevents?sendToAll=true");
-		from("undertow:ws://0.0.0.0:8080/api/carevents")
-			.log("Message received from Websocket : ${body}");
+		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic.gps}}?clientId=kafkaSseCamelClientx&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}")
+			//.log("GPS position received from Kafka : ${body}")
+			.setHeader(VertxWebsocketConstants.SEND_TO_ALL, constant("true"))
+			//.log(simple("${headers[CamelVertxWebsocket.sendToAll]}").toString())
+			.to("vertx-websocket://0.0.0.0:8080/api/carevents?lazyStartProducer=true");
+		 from("vertx-websocket://0.0.0.0:8080/api/carevents?")
+			.log("GPS position received from Websocket : ${body}");
 
 		// expose Bobbycar engine metrics from Kafka as Websocket
-		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic.metrics}}?clientId=kafkaSseMetricsClient&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}")
+		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic.metrics}}?clientId=kafkaSseMetricsClientx&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}")
 			.log("Metric received from Kafka : ${body}")
-			.to("undertow:ws://0.0.0.0:8080/api/carmetrics?sendToAll=true");
-		from("undertow:ws://0.0.0.0:8080/api/carmetrics")
-			.log("Message received from Websocket : ${body}");
+			.setHeader(VertxWebsocketConstants.SEND_TO_ALL, constant(true))
+			.to("vertx-websocket://0.0.0.0:8080/api/carmetrics");
+		from("vertx-websocket://0.0.0.0:8080/api/carmetrics")
+			.log("Metric received from Websocket : ${body}");
 
 		// expose Bobbycar zone change events from Kafka as Websocket
-		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic.zonechange}}?clientId=kafkaSseZoneChangeClient&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}")
-				.log("Zone change event received from Kafka : ${body}")
-				.to("undertow:ws://0.0.0.0:8080/api/zonechange?sendToAll=true");
-		from("undertow:ws://0.0.0.0:8080/api/zonechange")
-				.log("Message received from Websocket : ${body}");
+		from("kafka:{{com.redhat.bobbycar.camelk.kafka.topic.zonechange}}?clientId=kafkaSseZoneChangeClientx&brokers={{com.redhat.bobbycar.camelk.kafka.brokers}}")
+			.log("Zone change event received from Kafka : ${body}")
+			.setHeader(VertxWebsocketConstants.SEND_TO_ALL, constant(true))
+			.to("vertx-websocket://0.0.0.0:8080/api/zonechange");
+		from("vertx-websocket://0.0.0.0:8080/api/zonechange")
+			.log("Zone change event received from Websocket : ${body}");
 		
 	}
 	
