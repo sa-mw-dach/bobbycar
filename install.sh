935ccac90d66df9c6e663dc9fdfb07255a8f410c
Ortwin Schneider <ortwin.schneider@googlemail.com>
2023-07-11 09:49:55 +0200

    [Install] Update install script

--- install.sh
+++ install.sh
@@ -47,60 +47,71 @@ wait_for_operator() {
 source install_cleanup_vars.sh
 test -f .env && source .env
 
-# If weather api keys are empty, set a default value.
+#--------------------------
+# If Weather API keys are empty, set a default value.
+#--------------------------
 if [[ -z "$OWM_WEATHER_API_KEY" ]]; then
   OWM_WEATHER_API_KEY='my-owm-api-key';
-fi;
+fi
 if [[ -z "$IBM_WEATHER_API_KEY" ]]; then
   IBM_WEATHER_API_KEY='my-ibm-api-key';
-fi;
+fi
 
+#--------------------------
+# Installing OpenShift Serverless operator (optional)
+#--------------------------
 if [[ "$INSTALL_KNATIVE" == true ]]; then
-log "Installing OpenShift Serverless operator"
-oc apply -f config/operators/serverless-operator-subscription.yaml
-sleep 25
-log "Waiting for OpenShift Serverless operator"
-oc -n openshift-serverless wait --for=condition=Available deployment -l operators.coreos.com/serverless-operator.openshift-serverless --timeout=300s
-log "Installing Knative Serving config"
-oc apply -f config/knative/knative-serving.yaml
-log "Installing Knative Eventing config"
-oc apply -f config/knative/knative-eventing.yaml
-log "Installing Knative Kafka config"
-oc apply -f config/knative/knative-kafka.yaml
-sleep 10
-oc wait --for=condition=Ready knativeservings.v1beta1.operator.knative.dev/knative-serving --timeout 500s -n knative-serving
-oc wait --for=condition=Ready knativeeventings.v1beta1.operator.knative.dev/knative-eventing --timeout 500s -n knative-eventing
-oc wait --for=condition=Ready knativekafkas.v1alpha1.operator.serverless.openshift.io/knative-kafka --timeout 500s -n knative-eventing
-log "OpenShift Serverless Installation has been completed!!"
-fi ;
+  log "Installing OpenShift Serverless operator"
+  oc apply -f config/operators/serverless-operator-subscription.yaml
+  sleep 25
+  log "Waiting for OpenShift Serverless operator"
+  oc -n openshift-serverless wait --for=condition=Available deployment -l operators.coreos.com/serverless-operator.openshift-serverless --timeout=300s
+  log "Installing Knative Serving config"
+  oc apply -f config/knative/knative-serving.yaml
+  log "Installing Knative Eventing config"
+  oc apply -f config/knative/knative-eventing.yaml
+  log "Installing Knative Kafka config"
+  oc apply -f config/knative/knative-kafka.yaml
+  sleep 10
+  oc wait --for=condition=Ready knativeservings.v1beta1.operator.knative.dev/knative-serving --timeout 500s -n knative-serving
+  oc wait --for=condition=Ready knativeeventings.v1beta1.operator.knative.dev/knative-eventing --timeout 500s -n knative-eventing
+  oc wait --for=condition=Ready knativekafkas.v1alpha1.operator.serverless.openshift.io/knative-kafka --timeout 500s -n knative-eventing
+  log "OpenShift Serverless Installation has been completed!!"
+fi
 
+#--------------------------
+# Installing namespaced operators: Camel-K, Datagrid, AMQ Stream, AMQ Broker is optional in case of Drogue deployment
+#--------------------------
 if [[ "$INSTALL_OPERATORS" == true ]]; then
-log "Creating namespace $NAMESPACE for Bobbycar demo"
-oc new-project "$NAMESPACE" || true
-log "Installing operator group"
-sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/operator-group.yaml | oc apply -f -
-if [[ "$DROGUE_IOT" != true ]]; then
-  log "Installing the AMQ Broker operator"
-  sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/amq-operator-subscription.yaml | oc apply -f -
+  log "Creating namespace $NAMESPACE for Bobbycar demo"
+  oc new-project "$NAMESPACE" || true
+  log "Installing operator group"
+  sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/operator-group.yaml | oc apply -f -
+  if [[ "$DROGUE_IOT" != true ]]; then
+    log "Installing the AMQ Broker operator"
+    sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/amq-operator-subscription.yaml | oc apply -f -
+  fi
+  log "Installing the AMQ Streams operator"
+  sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/amq-streams-operator-subscription.yaml | oc apply -f -
+  log "Installing the Datagrid operator"
+  sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/datagrid-subscription.yaml | oc apply -f -
+  log "Installing the Camel-K operator"
+  sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/camel-k-operator-subscription.yaml | oc apply -f -
+
+  #--------------------------
+  # Waiting until all operator deployments are available
+  #--------------------------
+  if [[ "$DROGUE_IOT" != true ]]; then
+    wait_for_operator "AMQ Broker" amq-broker-rhel8
+  fi
+  wait_for_operator "AMQ Streams" amq-streams
+  wait_for_operator "Datagrid" datagrid
+  wait_for_operator "Camel-K" red-hat-camel-k
 fi
-log "Installing the AMQ Streams operator"
-sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/amq-streams-operator-subscription.yaml | oc apply -f -
-log "Installing the Datagrid operator"
-sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/datagrid-subscription.yaml | oc apply -f -
-log "Installing the Camel-K operator"
-sed "s:{{NAMESPACE}}:$NAMESPACE:g" config/operators/camel-k-operator-subscription.yaml | oc apply -f -
 
-# wait for operators
-
-if [[ "$DROGUE_IOT" != true ]]; then
-  wait_for_operator "AMQ Broker" amq-broker-rhel8
-fi
-wait_for_operator "AMQ Streams" amq-streams
-wait_for_operator "Datagrid" datagrid
-wait_for_operator "Camel-K" red-hat-camel-k
-
-fi
-
+#--------------------------
+# Installing the Infra Helm chart, Drogue Cloud is optional 
+#--------------------------
 log "Installing the infra Helm release: $HELM_INFRA_RELEASE_NAME"
 INFRA_OPTS=("-f" "helm/bobbycar-core-infra/values.yaml")
 if [[ "$DROGUE_IOT" == true ]]; then
@@ -118,9 +129,12 @@ helm upgrade --install "$HELM_INFRA_RELEASE_NAME" helm/bobbycar-core-infra/ \
 
 sleep 30
 
+#--------------------------
+# Waiting until infra components are running 
+#--------------------------
 if [[ "$DROGUE_IOT" != true ]]; then
-log "Waiting for AMQ Broker pod"
-oc wait --for=condition=Ready pod/bobbycar-amq-mqtt-ss-0 --timeout 300s
+  log "Waiting for AMQ Broker pod"
+  oc wait --for=condition=Ready pod/bobbycar-amq-mqtt-ss-0 --timeout 300s
 fi
 log "Waiting for Datagrid pod"
 oc wait --for=condition=Ready pod/bobbycar-dg-0 --timeout 300s
@@ -129,6 +143,9 @@ oc wait --for=condition=Ready pod/bobbycar-cluster-kafka-0 --timeout 300s
 log "Waiting for Kafka Bridge pod"
 [[ "$DROGUE_IOT" != true ]] && oc wait --for=condition=Available deployment/bobbycar-bridge --timeout 300s
 
+#--------------------------
+# Installing the Applications Helm Chart
+#--------------------------
 log "Installing the apps Helm release: $HELM_APP_RELEASE_NAME"
 APPS_OPTS=("-f" "helm/bobbycar-core-apps/values.yaml")
 if [[ "$DROGUE_IOT" == true ]]; then
@@ -147,6 +164,9 @@ helm upgrade --install "$HELM_APP_RELEASE_NAME" helm/bobbycar-core-apps \
 
 sleep 30
 
+#--------------------------
+# Waiting until some core app components are running 
+#--------------------------
 log "Waiting for Bobbycar pod"
 oc wait --for=condition=Available deployment/car-simulator --timeout 300s
 log "Waiting for Bobbycar Dashboard pod"
@@ -154,15 +174,25 @@ oc wait --for=condition=Available dc/dashboard --timeout 300s
 log "Waiting for Dashboard Streaming service pod"
 oc wait --for=condition=Available deployment/dashboard-streaming --timeout 300s
 
+#--------------------------
+# Installing the Serverless Helm Chart
+#--------------------------
 log "Installing the Serverless Helm release: $HELM_SERVERLESS_RELEASE_NAME"
 helm upgrade --install "$HELM_SERVERLESS_RELEASE_NAME" helm/bobbycar-opt-serverless \
 --set-string namespace="$NAMESPACE" \
---set-string otaServer.url="http://ota-server-bobbycar.$APP_DOMAIN"
+--set drogueIoT="$DROGUE_IOT" \
+--set-string otaServer.url="http://ota-server-$NAMESPACE.$APP_DOMAIN"
 
+#--------------------------
+# Waiting until Camel-K integrations are build and running (takes around 5-10 minutes) 
+#--------------------------
 log "Waiting for Camel-K integrations to complete..."
 oc wait --for=condition=Ready integration/cache-service --timeout 1800s
 oc wait --for=condition=Ready integration/kafka2datagrid --timeout 1800s
 [[ "$DROGUE_IOT" != true ]] && oc wait --for=condition=Ready integration/mqtt2kafka --timeout 1800s
 
+#--------------------------
+# Done! 
+#--------------------------
 log "Installation completed! Open the Bobbycar dashboard and get started:"
 oc get route dashboard -o json | jq -r .spec.host
